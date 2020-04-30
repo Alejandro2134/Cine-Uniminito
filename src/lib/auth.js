8f373704962ca7304be31874f78d4cf487642f87
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-04-30 18:00:50 -0500

    auth change

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,6 +12,6 @@ module.exports = {
         if(!req.isAuthenticated()){
             return next();
         }
-        return res.redirect('/peliculas');
+        return res.redirect('/multiplex');
     }
 }
